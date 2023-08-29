a1d75f19ebae67e95de6fc9863a5d71c1057bc6c
Karthik07nk <117428424+Karthik07nk@users.noreply.github.com>
2023-08-29 16:26:55 +0530

    Fix line endings

---
+++ index.js
@@ -0,0 +1,19 @@
+
+    var randomNumber1=Math.floor(Math.random()*6)+1;
+    var randomdice1="images/dice" +randomNumber1+ ".png";
+    document.querySelectorAll("img")[0].setAttribute('src',randomdice1);
+
+    var randomNumber2=Math.floor(Math.random()*6)+1;
+var randomdice2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[1].setAttribute('src',randomdice2)
+   
+    
+    if(randomNumber1>randomNumber2) {
+            document.querySelector("h1").innerHTML = "🚩Player 1 wins!";
+    }else if(randomNumber2 > randomNumber1){
+        document.querySelector("h1").innerHTML= "🚩Player 2 wins!";
+    }
+    else{
+            document.querySelector("h1").innerHTML="Draw!";
+    }
+
